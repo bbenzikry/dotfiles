7be536e7f648820281ccfb02d7a90491c1518d09
Beni Ben Zikry <bbenzikry@gmail.com>
2021-10-09 18:23:16 +0300

    fix go tip profile, add flux and istio to tool versions

--- profile/envs/go.zsh
+++ profile/envs/go.zsh
@@ -1,15 +1,15 @@
 #!usr/bin/env zsh
 export GOPATH="$XDG_DATA_HOME/go"
-export GOROOT="$(asdf where golang)/go"
+# export GOROOT="$(asdf where golang )/go"
 
-# export GOOS="darwin"
-# export GOARCH="amd64"
+export GOOS="darwin"
+export GOARCH="amd64"
 export GOPROXY=direct
 
 # gotip if available
 if "$GOPATH/sdk/gotip/bin/go" version >/dev/null 2>&1; then
 	export GOROOT="$GOPATH/sdk/gotip"
-	export GOTOOLDIR="$GOROOT/pkg/tool/linux_amd64"
+	export GOTOOLDIR="$GOROOT/pkg/tool/darwin_amd64"
 fi
 
 
