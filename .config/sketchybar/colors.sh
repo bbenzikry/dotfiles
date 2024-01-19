e8edc2ecd2ec351ae83c287f3117c32c4d9990ca
Beni Ben Zikry <bbenzikry@gmail.com>
2024-01-19 12:49:28 +0200

    Sketchybar + borders

---
+++ .config/sketchybar/colors.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+### Sonokai
+# export BLACK=0xff181819
+# export WHITE=0xffe2e2e3
+# export RED=0xfffc5d7c
+# export GREEN=0xff9ed072
+# export BLUE=0xff76cce0
+# export YELLOW=0xffe7c664
+# export ORANGE=0xfff39660
+# export MAGENTA=0xffb39df3
+# export GREY=0xff7f8490
+# export TRANSPARENT=0x00000000
+# export BG0=0xff2c2e34
+# export BG1=0xff363944
+# export BG2=0xff414550
+
+### Snazzy
+export BLACK=0xff181819
+export WHITE=0xffe2e2e3
+export RED=0xffff5c57
+export GREEN=0xff5af78e
+export BLUE=0xff57c7ff
+export YELLOW=0xfff3f99d
+export ORANGE=0xffff9f43
+export MAGENTA=0xffff6ac1
+export GREY=0xffe2e4e5
+export TRANSPARENT=0x00000000
+export BG0=0xff2c2e34
+export BG1=0xff363944
+export BG2=0xff414550
+
+### Catppuccin
+# export BLACK=0xff181926
+# export WHITE=0xffcad3f5
+# export RED=0xffed8796
+# export GREEN=0xffa6da95
+# export BLUE=0xff8aadf4
+# export YELLOW=0xffeed49f
+# export ORANGE=0xfff5a97f
+# export MAGENTA=0xffc6a0f6
+# export GREY=0xff939ab7
+# export TRANSPARENT=0x00000000
+# export BG0=0xff1e1e2e
+# export BG1=0x603c3e4f
+# export BG2=0x60494d64
+
+# General bar colors
+export BAR_COLOR=$BG0
+export BAR_BORDER_COLOR=$TRANSPARENTs
+export BACKGROUND_1=$BG1
+export BACKGROUND_2=$BG2
+export ICON_COLOR=$WHITE # Color of all icons
+export LABEL_COLOR=$WHITE # Color of all labels
+export POPUP_BACKGROUND_COLOR=$BAR_COLOR
+export POPUP_BORDER_COLOR=$WHITE
+export SHADOW_COLOR=$BLACK
